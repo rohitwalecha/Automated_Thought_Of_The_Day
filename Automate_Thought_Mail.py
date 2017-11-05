3bf547a568e5fd5a453f42a5220766dbf9d059b9
rohit <rohitwalecha8@gmail.com>
2017-11-05 20:58:03 +0530

    added enter

--- Automate_Thought_Mail.py
+++ Automate_Thought_Mail.py
@@ -6,5 +6,6 @@ def startAutomatingMail(urlOfQuoteOfTheDay):
     print("Starting..... Automating thought of the day Mail")
     Sending_an_image_through_mail.automateThought(urlOfQuoteOfTheDay)
     threading.Timer(20*60.0,startAutomatingMail).start()
+
     
 
