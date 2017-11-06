83b8a971d514a5d0e5ac2678db9a4144bf0904d1
rohit <rohitwalecha8@gmail.com>
2017-11-06 10:54:11 +0530

    Added some debugging statements to find out what we are getting in the links fetched from brainyquotes

--- ScrapingTheWeb2_Replica.py
+++ ScrapingTheWeb2_Replica.py
@@ -1,3 +1,6 @@
+from random import random, randint
+from turtledemo.forest import randomfd
+
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
@@ -20,14 +23,18 @@ def scrapeTheWeb() :
     #print(page_html)
     #Now using BeautifulSoup to read data systematically
     page_soup=soup(page_html,"html.parser")
+    #print(page_soup.img.get("src"))
     print("Web Scrapper : bs4 parsing the HTML Page")
     #print(page_soup.prettify())
     #print(page_soup.find_all("a"))
     links=page_soup.find_all("img")
+    for link in links :
+        print(link.get("src"),":",link.get("alt"))
     print("Web Scrapper : bs4 fetched all the img tags")
-    print("https://www.brainyquote.com"+links[2].get("src"))
+    #print("https://www.brainyquote.com"+links[2].get("src"))
     print("Web Scrapper : Image Scrapped Successfully")
-    SendingAnImageThroughMail2.automateThought("https://www.brainyquote.com" + links[2].get("src"))
+    print("https://www.brainyquote.com"+links[randint(2,10)].get("src"))
+    SendingAnImageThroughMail2.automateThought("https://www.brainyquote.com" + links[randint(2,10)].get("src"))
     threading.Timer(60*60.0,scrapeTheWeb).start()
     #https://www.brainyquote.com/photos_tr/en/s/sophocles/101515/sophocles1-2x.jpg
 
