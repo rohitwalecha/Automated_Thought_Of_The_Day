4f65252751fe8ac8c0cdc24389d435c6b27adf36
rohit <rohitwalecha8@gmail.com>
2017-11-12 00:23:55 +0530

    Added some debugging statements to find out what we are getting in the links fetched from brainyquotes

--- ScrapingTheWeb2_Replica.py
+++ ScrapingTheWeb2_Replica.py
@@ -1,6 +1,5 @@
 from random import random, randint
 from turtledemo.forest import randomfd
-
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
@@ -8,18 +7,36 @@ import urllib.request
 #import Automate_Thought_Mail
 import threading
 import SendingAnImageThroughMail2
+from selenium import  webdriver
+from selenium.webdriver.common.keys import Keys
+import  time
 
 class AppURLopener(urllib.request.FancyURLopener):
     version = "Mozilla/5.0"
 
+
 def scrapeTheWeb() :
-    print("Web Scrapper : Intializing....")
-    opener = AppURLopener()
-    response = opener.open('https://www.brainyquote.com/topics/inspirational')
-    print("Web Scrapper : Going to www.brainyquote.com to fetch a random qoute of the day")
-    #Reading the object of response and printing the html of above url of web page
-    print("Web Scrapper : Reading the response object")
-    page_html=response.read()
+
+    print("Selenium Web Driver Scroller : Scrolling to the end of web page : https://www.brainyquote.com/topics/inspirational")
+    browser=webdriver.Chrome("/Users/rohit/Downloads/chromedriver");
+    browser.get("https://www.brainyquote.com/topics/inspirational")
+    time.sleep(1)
+    elem=browser.find_element_by_tag_name("body")
+    numberOfPageDowns=100
+    while numberOfPageDowns :
+        elem.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.2)
+        numberOfPageDowns-=1
+        print("Selenium Web Driver Scroller : scrolling")
+    print("Selenium Web Driver Scroller : sScrolling to the end completed")
+    page_html=browser.page_source
+     # print("Web Scrapper : Intializing....")
+    # opener = AppURLopener()
+    # response = opener.open('https://www.brainyquote.com/topics/inspirational')
+    # print("Web Scrapper : Going to www.brainyquote.com to fetch a random qoute of the day")
+    # #Reading the object of response and printing the html of above url of web page
+    # print("Web Scrapper : Reading the response object")
+    # page_html=response.read()
     #print(page_html)
     #Now using BeautifulSoup to read data systematically
     page_soup=soup(page_html,"html.parser")
@@ -33,10 +50,12 @@ def scrapeTheWeb() :
     print("Web Scrapper : bs4 fetched all the img tags")
     #print("https://www.brainyquote.com"+links[2].get("src"))
     print("Web Scrapper : Image Scrapped Successfully")
-    print("https://www.brainyquote.com"+links[randint(2,10)].get("src"))
-    SendingAnImageThroughMail2.automateThought("https://www.brainyquote.com" + links[randint(2,10)].get("src"))
+    randomInteger=randint(2,109)
+    print(randomInteger)
+    SendingAnImageThroughMail2.automateThought("https://www.brainyquote.com" + links[randomInteger].get("src"))
     threading.Timer(60*60.0,scrapeTheWeb).start()
     #https://www.brainyquote.com/photos_tr/en/s/sophocles/101515/sophocles1-2x.jpg
+
 
 
 scrapeTheWeb()
